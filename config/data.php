92c0af66191897b7745832c2e81b3397927a2d26
ArianaCisternas <ariana.cisternas@uc.cl>
2022-12-01 19:52:09 -0300

    agregar config  data por bug

---
+++ config/data.php
@@ -0,0 +1,10 @@
+<?php
+#config/data.php
+$user = 'grupo108';
+$password = 'bbdd.39';
+$databaseName = 'grupo39e3';
+
+$user2 = 'grupo108';
+$password2 = 'Completo123';
+$databaseName2 = 'grupo108e3';
+?>
