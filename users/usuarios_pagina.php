dcacae13be81cbe525569c0e284f52d56cd50afa
Fran <francisca.aguayo@uc.cl>
2022-12-02 18:13:11 -0300

    probar require

--- users/usuarios_pagina.php
+++ users/usuarios_pagina.php
@@ -8,7 +8,7 @@
 
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conexion.php");
+  require("..config/conexion.php");
 
 	$query = "SELECT *
 	FROM usuarios;";
