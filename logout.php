c2e04ff221becf48559ea6c98fdc0d3238e20e39
Fran <francisca.aguayo@uc.cl>
2022-12-02 18:16:57 -0300

    probar require

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_SESSION['name']))
-                {
-                unset($_SESSION['name']);
-                }
-                echo '
-You have been successfully logout
-';
-?>
