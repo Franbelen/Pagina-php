9829868ab94b5e1d6bcb90ec3d419789224987a5
ArianaCisternas <ariana.cisternas@uc.cl>
2022-12-02 20:57:52 -0300

    copiar conexion wiki

--- config/conexion.php
+++ config/conexion.php
@@ -1,17 +1,15 @@
-<?php
-  try {
-    #Pide las variables para conectarse a la base de datos.
-    require('data.php'); 
-    # Se crea la instancia de PDO
-    $db = new PDO("pgsql:dbname=$database;host=localhost;port=5432;user=$user;password=$password");
-    $db2 = new PDO("pgsql:dbname=$database2;host=localhost;port=5433;user=$user2;password=$password2");
-  } catch (Exception $e) {
-    echo "No se pudo conectar a la base de datos: $e";
-  }
-  #try {
-    #require('data.php');
-    #$db2 = new PDO("pgsql:dbname=$database2;host=localhost;port=5433;user=$user2;password=$password2");
-    #} catch (Exception $e) {
-      #echo "No se pudo conectar a la base de datos: $e";
-   # }
+
+?><?php
+try {
+  $user = 'grupo39';
+  $password = 'bbdd.39';
+  $databaseName = 'grupo39e3';
+  $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+  $user2 = 'grupo108';
+  $password2 = 'Completo123';
+  $databaseName2 = 'grupo108e3';
+  $db2 = new PDO("pgsql:dbname=$databaseName2;host=localhost;port=5432;user=$user2;password=$password2");
+} catch (Exception $e) {
+  echo "No se pudo conectar a la base de datos: $e";
+}
 ?>
