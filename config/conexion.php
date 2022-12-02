25118121cf945e6d35b685142ae1d5f84ab70c58
ArianaCisternas <ariana.cisternas@uc.cl>
2022-12-02 20:41:54 -0300

    cambiar conexion a un solo try

--- config/conexion.php
+++ config/conexion.php
@@ -4,13 +4,14 @@
     require('data.php'); 
     # Se crea la instancia de PDO
     $db = new PDO("pgsql:dbname=$database;host=localhost;port=5432;user=$user;password=$password");
+    $db2 = new PDO("pgsql:dbname=$database2;host=localhost;port=5433;user=$user2;password=$password2");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
   }
-  try {
-    require('data.php');
-    $db2 = new PDO("pgsql:dbname=$database2;host=localhost;port=5433;user=$user2;password=$password2");
-    } catch (Exception $e) {
-      echo "No se pudo conectar a la base de datos: $e";
-    }
+  #try {
+    #require('data.php');
+    #$db2 = new PDO("pgsql:dbname=$database2;host=localhost;port=5433;user=$user2;password=$password2");
+    #} catch (Exception $e) {
+      #echo "No se pudo conectar a la base de datos: $e";
+   # }
 ?>
